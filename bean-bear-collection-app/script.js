3b13f489ff7162eec92b4984e4bad4174c728b77
Gordon Karlsson <gpkarlsson72@gmail.com>
2026-02-03 17:21:41 -0600

    working on data formatting, added isOwned checkbox, added generations to data objects

--- bean-bear-collection-app/script.js
+++ bean-bear-collection-app/script.js
@@ -1,7 +1,3 @@
-// console.log("All generation keys:", Object.keys(bearData.generations));
-// console.log("gen1 data:", bearData.generations.gen1);
-// console.log("gen2 data:", bearData.generations.gen2);
-console.log('Bootstrap loaded?', typeof bootstrap !== 'undefined');
 const ownedCheckbox = document.getElementById('collectCheckbox');
 const bearContainer = document.getElementById('bear-container');
 const allGenerations = bearData.generations;
@@ -11,7 +7,6 @@ for (const genKey in allGenerations) {
     const generationBears = allGenerations[genKey];
     allBearNames.push(generationBears);
 }
-
 
 //loop through each generation  
 Object.entries(bearData.generations).forEach(([genName, bearArray]) => {
@@ -30,31 +25,44 @@ const cardsContainer = document.createElement('div');
 cardsContainer.className = 'row';
 
 bearArray.forEach(bearData => {
+     console.log(`Processing bear ${genName}: ${bearData.bearName}`);
     const card = document.createElement('div');
     card.className = 'col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12 mb-3';
 
-    card.innerHTML = `
-<div class="card">
-    <img src="https://picsum.photos/200" class="card-img-top" alt="...">
-    <div class="card-body shadow p-3">
-        <h5 class="card-title ">${bearData.bearName}</h5>
-        <input type="checkbox" name="inCollection" id="collectCheckbox">
-        <label for="inCollection">Bear in collection</label>
-        <p class="card-text"><b>Birthday:</b> ${bearData.birthday}</p>
-        <p class="card-text"><b>Hangtag?</b> ${bearData.hasHangtag}</p>
-        <p class="card-text"><b>Venue:</b> ${bearData.venue}</p>
-        <p class="card-text"><b>Tour memory:</b> ${bearData.tourMemory}</p>
+    //TODO: add check for limited edition bears, only render total prod num if limited edition
+card.innerHTML = `
+    <div class="card">
+        <img src="https://picsum.photos/200" class="card-img-top" alt="...">
+        <div class="card-body shadow p-3">
+            <p class="cart-text"><b>Name:</b> ${bearData.bearName}</p>
+            <p class="card-text"><b>Birthday:</b> ${bearData.birthday}</p>
+            <p class="card-text"><b>Hangtag?</b> ${bearData.hasHangtag}</p>
+            <p class="card-text"><b>Venue:</b> ${bearData.venue}</p>
+            <p class="card-text"><b>Tour memory:</b> ${bearData.tourMemory}</p>
+            <p class="card-text"><b>Year Released:</b> ${bearData.year}</p>
+           <span class="badge ${bearData.inCollection} ? bg-success : bg-secondary}"> ${bearData.inCollection ? '✓ Owned' : 'Not Owned'} </span>
+        </div>
     </div>
-</div>
-`;
-
-    cardsContainer.appendChild(card)
+    `;
+    cardsContainer.appendChild(card);
 });
-genSection.appendChild(cardsContainer);
-bearContainer.appendChild(genSection);
+    genSection.appendChild(cardsContainer);
+    bearContainer.appendChild(genSection);
+     console.log(`Finished bear ${genName, bearData.bearName}`);
 });
 
 const bearCard = document.getElementsByClassName("card");
+
+Object.entries(bearData.generations).forEach(([genName, data]) => {
+  console.log(`${genName}:`, {
+    type: typeof data,
+    isArray: Array.isArray(data),
+    length: data.length,
+    firstItem: data[0],
+    firstItemType: typeof data[0]
+  });
+});
+
 
 // function handleInCollectionCheckbox(){
 //     if (ownedCheckbox.checked === false) {
